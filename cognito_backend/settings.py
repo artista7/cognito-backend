b95d0b4351e5a6b09aa78662dd3e11dc9a8f5269
Vivek Verma <vivekverma-policybazaar@bitbucket.ord>
2019-09-30 02:06:13 +0530

    Changes for CORS

--- cognito_backend/settings.py
+++ cognito_backend/settings.py
@@ -31,6 +31,18 @@ ALLOWED_HOSTS = ["*"]
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'X-Amz-Security-Token'
+]
 # Application definition
 
 INSTALLED_APPS = [
