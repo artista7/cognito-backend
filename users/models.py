d34f719ebbfb4a48a01b62f21e11d97b73124823
Vivek Verma <vivekverma-policybazaar@bitbucket.ord>
2019-10-11 15:19:39 +0530

    Added signup fix

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ import uuid
 from django.db import models
 import atnp.models as am
 from atnp.models import Student, College, Company
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 import datetime
 
 
@@ -18,15 +18,17 @@ class User(AbstractBaseUser):
     college = models.ForeignKey(College, on_delete=models.CASCADE, null=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
 
-    username = models.CharField(max_length=100 , unique=True)
+    username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(max_length=100)
     name = models.CharField(max_length=100)
     phoneNumber = models.CharField(max_length=100)
-    status = models.CharField(max_length=255)
-    createdAt = models.DateTimeField(db_column='createdAt', auto_now_add=True)  # Field name made lowercase.
-    updatedAt = models.DateTimeField(db_column='updatedAt', auto_now=True)  # Field name made lowercase.
+    status = models.CharField(max_length=255, default='active')
+    # Field name made lowercase.
+    createdAt = models.DateTimeField(db_column='createdAt', auto_now_add=True)
+    # Field name made lowercase.
+    updatedAt = models.DateTimeField(db_column='updatedAt', auto_now=True)
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = [ 'email', 'name', 'phoneNumber' ]
+    REQUIRED_FIELDS = ['email', 'name', 'phoneNumber']
 
     def __str__(self):              # __unicode__ on Python 2
         return self.username
